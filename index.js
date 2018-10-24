54fd11804f32a9e0c1117f40ddf184e088207316
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-10-24 22:46:56 +0000

    done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+function sumItems(arr) {
+  if (!Array.isArray(arr)) return NaN;
+  for (var value, total=0, x=0, iLen=arr.length; x<iLen; x++) {
+    value = arr[x];
+    total += Array.isArray(value)? sumItems(value) : value;
+  }
+  return total;
 }
-
 module.exports = sumItems;
